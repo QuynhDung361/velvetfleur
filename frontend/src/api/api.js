4943a0bf997ea198a798546c9accef5eea676718
QuynhDung361 <dungptqhe171569@fpt.edu.vn>
2025-05-24 15:20:51 +0700

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,16 @@
 import axios from "axios";
-const local = 'http://localhost:5000'
-const production = ''
+
+
+// const local = 'http://localhost:5000'
+// const production = ''
+// const api = axios.create({
+//     baseURL : `${local}/api`
+// })
+
+// Sử dụng biến môi trường để chọn đúng URL
 const api = axios.create({
-    baseURL : `${local}/api`
-})
+    baseURL: `${process.env.REACT_APP_API_BASE_URL}/api`
+});
 
 // Thêm interceptor để gửi token với mỗi request
 api.interceptors.request.use(
