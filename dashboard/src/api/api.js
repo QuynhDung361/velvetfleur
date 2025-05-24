f4822c0ce3862d06e4d88e75fb57c04070f8e71f
QuynhDung361 <dungptqhe171569@fpt.edu.vn>
2025-05-24 15:51:46 +0700

    Update api.js

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// const api = axios.create({
+//     baseURL : 'http://localhost:5000/api'
+// })
 const api = axios.create({
-    baseURL : 'http://localhost:5000/api'
-})
+    baseURL: `${process.env.REACT_APP_API_BASE_URL}/api`
+});
 
 export default api
